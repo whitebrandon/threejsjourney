1ce029068fd245987f59ce04f24b208f6db48055
Brandon <white.brandonsean@gmail.com>
2023-11-07 19:36:49 -0500

    added info to run dev env

---
+++ lessons/03/exercise/script.js
@@ -0,0 +1,8 @@
+import * as THREE from "three";
+
+
+// console.log('JavaScript is working!')
+// console.log(THREE);
+
+const scene = new THREE.Scene();
+const sphereGeometry = new THREE.SphereGeometry(1.5, 32, 32);
